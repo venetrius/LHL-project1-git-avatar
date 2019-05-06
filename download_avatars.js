476235463602122ed4f2311089779a44894d43cd
venetrius <gergelygjuhasz@gmail.com>
2019-05-06 23:22:36 +0000

    Step 6: Don't commit your secrets!

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,19 @@
 var request = require('request');
+var secrets = require('./secrets');
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' :  secrets.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery",function (err, body){console.log("hi");});
 
 console.log('Welcome to the GitHub Avatar Downloader!');
