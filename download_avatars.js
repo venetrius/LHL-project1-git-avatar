77d3d7da2fd0b3f5812c67c67092cd2ebb1d89c3
venetrius <gergelygjuhasz@gmail.com>
2019-05-06 22:49:57 +0000

    Start the getRepoContributors function

---
+++ download_avatars.js
@@ -0,0 +1,8 @@
+var request = require('request');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+console.log('Welcome to the GitHub Avatar Downloader!');
