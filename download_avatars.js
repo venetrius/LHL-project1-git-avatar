89247f48d5bb61d48633b46864f886d8170e2ce0
venetrius <gergelygjuhasz@gmail.com>
2019-05-06 23:38:20 +0000

    Step 7: Implement downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -20,4 +21,18 @@ getRepoContributors("jquery", "jquery",
   }
 );
 
+function downloadImageByURL(url, filePath) {
+  var req = request.get(url)
+          .on('error', function(err){throw err;})
+          .on('response', function(response){
+            console.log('Downloading image...');
+            console.log('Response status code:', response.statusCode);
+          })
+          .pipe(
+            fs.createWriteStream(filePath).on("finish",function() {console.log(filePath, "downloaded")}) // another way to call a callback
+          );
+}
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+downloadImageByURL("https://avatars2.githubusercontent.com/u/414129?v=4", "./this.jpg");
