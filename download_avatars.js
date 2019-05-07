2f476b4e32fba05d143e344ddb169fe4250a40af
venetrius <gergelygjuhasz@gmail.com>
2019-05-07 02:17:59 +0000

    add error handling

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization' :  process.env.GITHUB_TOKEN
+      'Authorization' : process.env.GITHUB_TOKEN
     }
   };
   request(options, function(err, res, body) {
@@ -29,31 +29,51 @@ function downloadImageByURL(url, filePath) {
 
 function createGetAvatarLocalPath(){
   var id = 0;
-  var path = './avatars/';
   var func = function(){
     return path + (id++) + ".jpg";
   };
   return func;
 }
 
-var getAvatarLocalPath = createGetAvatarLocalPath();
+function processJSON (err, body){
+  console.log(body.message);
+        if(body.message == "Not Found"){
+          console.log("unsuccessful request with repoOwner: " + repoOwner + " and repoName " + repoName);
+          return;
+        }else if(body.message && body.message.contains("exceeded") && body.message.contains("limit")){
+          console.log("error with authentication");
+          return;
+        }
 
+        body.map(a => a.avatar_url).forEach(a => downloadImageByURL(a, getAvatarLocalPath()));
+      }
+
+function isDirectoryOK(){
+  try {
+    var stats = fs.statSync(path);
+    return stats.isDirectory();
+  }catch(e){
+    return false;
+  }
+}
+
+var getAvatarLocalPath = createGetAvatarLocalPath();
+var path = './avatars/';
 console.log('Welcome to the GitHub Avatar Downloader!');
+if(process.argv.length > 4){
+  console.log("number of arguments: ", process.argv.length - 2, " only the first two will be used");
+}
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
 if(!(repoOwner && repoName)){
   console.log("repoOwner ", repoOwner, " or repoName: ", repoName, " is not valid");
+}else if(! process.env.GITHUB_TOKEN){
+  console.log(".env file is required with a GITHUB_TOKEN entry");
+else if(! isDirectoryOK()){
+  console.log('directory',path , " is not present");
+}else{
+  getRepoContributors(repoOwner, repoName, processJSON);
 }
-else{
-  getRepoContributors(repoOwner, repoName,
-    function (err, body){
-      if(body.message == "Not Found"){
-        console.log("unsuccessful request with repoOwner: " + repoOwner + " and repoName " + repoName);
-        return;
-      }
-      body.map(a => a.avatar_url).forEach(a => downloadImageByURL(a, getAvatarLocalPath()));
-    }
-  );
-}
+
